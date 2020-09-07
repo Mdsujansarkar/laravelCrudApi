1c3bfcaea83ff3b4a5acc43e9168ecd549654a93
Mdsujansarkar <sujansarkar786@gmail.com>
2020-09-07 16:35:01 +0600

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $customers = Customer::orderBy('id','desc')->get();
+        return response()->json($customers);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customers = new Customer;
+        $customers->first_name = $request->first_name;
+        $customers->last_name = $request->last_name;
+        $customers->email = $request->email;
+        $customers->save();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $customers = Customer::findOrFail($id);
+        return response()->json($request);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $customers = Customer::findOrFail($id);
+        $customers->first_name = $request->first_name;
+        $customers->last_name = $request->last_name;
+        $customers->email = $request->email;
+        $customers->save();
+        return response()->json($request);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $customers = Customer::findOrFail($id);
+        $customers->delete();
+        return response()->json($customers);
+    }
+}
